a9d8e4475fb102b6662e34959bc5c22ff73bbcd7
shao <370083084@qq.com>
2018-09-21 13:35:42 +0800

    微调时间

--- src/main/java/com/lm/jbm/thread/PeachThread.java
+++ src/main/java/com/lm/jbm/thread/PeachThread.java
@@ -39,8 +39,8 @@ public class PeachThread implements Runnable {
 				if(flag) { // 01:00 ~ 10:30，间隔6~12秒
 					sleepTime1 = RandomUtil.getRandom(3000, 6000);
 					sleepTime2 = RandomUtil.getRandom(3000, 6000);
-				} else { // 其他时间段， 间隔5~9秒
-					sleepTime1 = RandomUtil.getRandom(3000, 5000);
+				} else { // 其他时间段， 间隔4~8秒
+					sleepTime1 = RandomUtil.getRandom(2000, 5000);
 					sleepTime2 = RandomUtil.getRandom(2000, 3000);
 				}
 			} else if(way > 10 && way <= 20) { // 人少
