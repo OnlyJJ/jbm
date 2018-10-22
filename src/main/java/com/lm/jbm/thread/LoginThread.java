d6154945c2200db57925db5de793555e4a36e822
shao <370083084@qq.com>
2018-10-22 11:20:38 +0800

    优化自动签到

--- src/main/java/com/lm/jbm/thread/LoginThread.java
+++ src/main/java/com/lm/jbm/thread/LoginThread.java
@@ -1,10 +1,14 @@
 package com.lm.jbm.thread;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.lang.StringUtils;
 
 import com.lm.jbm.service.JmService;
+import com.lm.jbm.utils.PropertiesUtil;
 import com.lm.jbm.utils.RandomUtil;
 
 
@@ -23,29 +27,48 @@ public class LoginThread implements Runnable {
 	public void run() {
 		while(true) {
 			try {
-				String[] userIds = RandomUtil.getUserIds("userId");
-				if(userIds != null) {
-					for(String userId : userIds) {
-						String ip = "";
-						if(ipMap.contains(userId)) {
-							ip = ipMap.get(userId);
-						} else {
-							ip = RandomUtil.getIp();
-							ipMap.put(userId, ip);
-						}
-						String ret = JmService.login(userId, RandomUtil.getPwd(), ip);
-						if(StringUtils.isNotEmpty(ret)) {
-							serssionMap.put(userId, ret);
-							if(!signMap.contains(userId)) {
-								signMap.put(userId, "1");
-								JmService.sign(userId, ret, ip);
-								Thread.sleep(1000);
-							}
-						}
-						Thread.sleep(2000);
-					}
+				List<String> list = new ArrayList<String>();
+				if(PropertiesUtil.getLevelUserIds(1) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(1));
 				}
-				Thread.sleep(1000*60*10);
+				if(PropertiesUtil.getLevelUserIds(2) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(2));
+				}
+				if(PropertiesUtil.getLevelUserIds(3) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(3));
+				}
+				if(PropertiesUtil.getLevelUserIds(4) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(4));
+				}
+				if(PropertiesUtil.getLevelUserIds(5) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(5));
+				}
+				if(PropertiesUtil.getLevelUserIds(6) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(6));
+				}
+				if(PropertiesUtil.getLevelUserIds(7) != null) {
+					list.addAll(PropertiesUtil.getLevelUserIds(7));
+				}
+				for(String userId : list) {
+					String ip = "";
+					if(ipMap.contains(userId)) {
+						ip = ipMap.get(userId);
+					} else {
+						ip = RandomUtil.getIp();
+						ipMap.put(userId, ip);
+					}
+					String ret = JmService.login(userId, RandomUtil.getPwd(), ip);
+					if(StringUtils.isNotEmpty(ret)) {
+						serssionMap.put(userId, ret);
+						if(!signMap.contains(userId)) {
+							signMap.put(userId, "1");
+							JmService.sign(userId, ret, ip);
+							Thread.sleep(3000);
+						}
+					}
+					Thread.sleep(5000);
+				}
+				Thread.sleep(1000*60*30);
 			} catch (Exception e) {
 				break;
 			}
